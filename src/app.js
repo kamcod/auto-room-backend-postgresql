78b7c61661484ce29a37d192dee7c03c02df2217
kamcod <mkamran4786@gmail.com>
2023-10-29 01:20:11 +0500

    some useful and security packages in project

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port =  process.env.PORT || 3000;
 
+const cors = require('cors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
@@ -13,10 +18,16 @@ const registerRoutes = require('./routes/register')
 const jobsRoutes = require('./routes/jobs')
 // const webHooksRoute = require('./routes/webHooks');
 const authentication = require('./middlewares/authentication')
-//
-//
+
 const errorHandler = require('./middlewares/error-handler')
 const notFound = require('./middlewares/not-found')
+
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
+);
 
 app.use(express.json())
 
@@ -30,6 +41,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // for parsing multipart/form-data
 app.use(upload.array());
 
+app.use(cookieParser());
+app.use(xss())
+app.use(cors({ credentials: true, origin: process.env.frontend_domain }))
+app.use(helmet())
 
 app.use('/api', registerRoutes)
 app.use('/api', authentication, jobsRoutes)
